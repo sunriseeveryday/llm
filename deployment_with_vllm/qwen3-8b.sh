eb10c22e8c4fe060d7c72ebc1e540c3a131e1142
qitao.li <qitao.li@nio.com>
2025-06-10 17:39:02 +0800

    update

---
+++ deployment_with_vllm/qwen3-8b.sh
@@ -0,0 +1,13 @@
+vllm serve Qwen/Qwen3-8B \
+  --host 0.0.0.0 \
+  --port 8000 \
+  --uvicorn-log-level "info" \
+  --enable-reasoning \
+  --reasoning-parser deepseek_r1 \
+  --enable-auto-tool-choice \
+  --tool-call-parser hermes \
+  --trust-remote-code \
+  --max-model-len 10240 \
+  --enable-metrics \
+  --metrics-port 8001 \
+  --tensor_parallel_size 8
